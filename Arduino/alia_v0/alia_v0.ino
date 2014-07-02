6b1abe6e5e9d2bfc27efa88ee671f619e879b947
mpinner <mpinner@gmail.com>
2014-07-02 12:50:05 -0700

    add BOM and datasheet links

--- Arduino/alia_v0/alia_v0.ino
+++ Arduino/alia_v0/alia_v0.ino
@@ -1,58 +1,95 @@
-/* Alia: Bringing back human connection (2nd place winner of #ExtremeWearables designathon)
- author: matt pinner @mpinner
- intent: connect exactly two people with wearable devices via xbees.
- FuelGauge: shows the sensors readings of the other person's device 
- Emote : recieve a message to the other
- Nudge : send a message to the other
- Sense : read local sensors
- Update : send sensor reading
+/* Alia: sharing accountability and bringing back human connection 
+ (2nd place winner of #ExtremeWearables designathon)
+ Author: matt pinner @mpinner (and team)
+ 
+ Intent: connect exactly two people with wearable devices via xbees.
+ 
+ Terms:
+ fuelGauge: shows the sensors readings of the other person's device 
+ emote : recieve a message to the other
+ nudge : send a message to the other
+ sense : read and send local sensors
+ update : send sensor reading
+ 
+ PINS:
+ 0/1 : tx/rx for Serial over XBee 
+ 2   : nudge button (INPUT_PULLUP)
+ 4/5 : adxl345 accelerometer (i2c)
+ 6   : neopixel x8 stick (ws2811)
+ 13  : vibration motor and onboard led (high/low modulated for power savings)
+ 
+ BOM: https://www.sparkfun.com/wish_lists/90529
+ fio: http://arduino.cc/en/Main/ArduinoBoardFio
+ xbee: https://www.sparkfun.com/datasheets/Wireless/Zigbee/XBee-Datasheet.pdf
+ battery: https://www.sparkfun.com/datasheets/Batteries/UnionBattery-1000mAh.pdf 
+ button:https://www.sparkfun.com/datasheets/Components/General/00097.jpg
+ accelerometer: https://www.sparkfun.com/datasheets/Sensors/Accelerometer/ADXL345.pdf
+ leds: https://dlnmh9ip6v2uc.cloudfront.net/datasheets/Components/LED/adafruit-neopixel-uberguide.pdf
+ vibrator: https://www.sparkfun.com/datasheets/Robotics/310-101_datasheet.pdf
  
  */
- 
+
+
 #include <Bounce2.h>
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
-
-#define INITIALSTATE 49
-
+// this NEEDS to be false for anything to work in a P2P mode. 
+// this can be on for testing against a Serial monitor
 #define DEBUG false
 
+#define BUTTON_PIN 2
+#define LEDS_PIN 6
+#define VIBE_PIN 13
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+// We're sending BYTES for our given sensor readings and to nudge
+// we save the state of our current reading to send so eventually we can do smoothing
+// this defines the initial state for the CONNECTED reading and assumes no data is available
+#define INITIALSTATE 49
+#define NUDGEMSG 48
 
 
-int speakerPin = 13;	// speaker connected to digital pin 9
 
+// setup the led strip
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, LEDS_PIN, NEO_GRB + NEO_KHZ800);
+
+// we're currently faking readings for the demo
+// we do this by pretending our readings are ever increasing
+// this defines how quickly those readings should increase and tracks the last time of increase.
 int increaseRate = 30000;
 long lastincrease = increaseRate;
 
 
+// saves the last incoming message from our peer
+// 
 char inByte = 48;         // incoming serial byte
 
+// tracks our most recently sent data
+// ideally this is derived from the current sensor reading with some smoothing
 char state = INITIALSTATE;
 
-#define BUTTON_PIN 2
-#define LED_PIN 13
 
-
-
-// Instantiate a Bounce object
+// Instantiate a Bounce object. this makes using buttons happy.
+// not sure this is really needed in this case.
 Bounce debouncer = Bounce(); 
 
+
+
+// THIS STUFF HAPPENS EXACTLY ONCE UPON INITIAL STARTUP
 void setup()
 {
-  // Setup the button
+
+  // Setup the button and pull high (hint: this simplifies the electronics)
   pinMode(BUTTON_PIN,INPUT_PULLUP);
 
   // After setting up the button, setup debouncer
   debouncer.attach(BUTTON_PIN);
   debouncer.interval(5);
 
-  //Setup the LED
-  pinMode(LED_PIN,OUTPUT);
+  //Setup the VIBRATION MOTOR
+  pinMode(VIBE_PIN,OUTPUT);
 
-  // start serial port at 9600 bps and wait for port to open:
+  // start serial port at 57600 bps and wait for port to open
+  // this is also the required baud rate for wireless programming so keep it here
   Serial.begin(57600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
@@ -63,30 +100,35 @@ void setup()
   establishContact();  // send a byte to establish contact until receiver responds 
 }
 
+
+// THIS STUFF HAPPENS OVER AND OVER AS FAST AS POSSIBLE
+// hint: there is a bit of a delay in the end to avoid over saturating the serial buffers
 void loop()
 {
-  debouncer.update();
+  // not used yet...
+  //debouncer.update();
   //int value = debouncer.read();
 
-  // Turn on or off the LED
+
+  // Detect user's nudge
   if ( digitalRead(BUTTON_PIN) == HIGH) {
-    // Serial.println('0');
+    // button is not pressed, do nothing
   } 
   else {
 
-    // button press
+    // button pressed
     if(DEBUG)  Serial.println("button press");
 
-    Serial.write(48);
-
+    // nudge our peer and react locally
     nudge();
-
 
   }
 
-  // if we get a valid byte, read analog ins:
+
+  // if we see data from our peer do stuff
   if (Serial.available() > 0) {
-    // get incoming byte:
+
+    // save incoming byte data
     inByte = Serial.read();
 
     if(DEBUG) {
@@ -94,7 +136,8 @@ void loop()
       Serial.println(inByte);
     }
 
-    if ('0' == inByte) {
+    // if they send us a nudge
+    if (NUDGEMSG == inByte) {
       emote();
     } 
 
@@ -103,7 +146,64 @@ void loop()
 
   }
 
+  // read and send local sensed data
   sense();
+
+
+  //avoids over saturating the serial buffers
+  delay(250);
+
+  return;
+} // END OF loop()
+
+
+// emote : recieve a nudge from the other
+void emote() {
+
+  lightEmUp(8);
+  beep(VIBE_PIN,50,145);	
+  off();	
+  beep(VIBE_PIN,100,145);
+  lightEmUp(8);
+
+  beep(VIBE_PIN,200,145);	
+  off();	
+  beep(VIBE_PIN,400,145);	
+  lightEmUp(8);
+  beep(VIBE_PIN,800,145);	
+  beep(VIBE_PIN,1600,1000);
+  off();	
+
+  state=INITIALSTATE;
+
+  return;
+
+}
+
+
+// nudge our peer and react locally
+void nudge() {
+
+
+  // send peer our nudge 
+  Serial.write(NUDGEMSG);
+
+  lightEmUp(8);
+  beep(VIBE_PIN,50,500);	
+  beep(VIBE_PIN,100,500);	
+  off();	
+
+
+  return;
+
+}
+
+
+// sense : read and send local sensors
+void sense() {
+  // send sensor values:
+  //if (DEBUG) 
+  Serial.write(state);
 
   if ((millis() - increaseRate) > lastincrease) {
     if(DEBUG) {
@@ -118,75 +218,39 @@ void loop()
       Serial.println(state);
     }
   }
-
-
-  delay(250);
-
-}
-
-void emote() {
-
-  lightEmUp(8);
-  beep(speakerPin,50,145);	
-  off();	
-  beep(speakerPin,100,145);
-  lightEmUp(8);
-
-  beep(speakerPin,200,145);	
-  off();	
-  beep(speakerPin,400,145);	
-  lightEmUp(8);
-  beep(speakerPin,800,145);	
-  beep(speakerPin,1600,1000);
-  off();	
-
-  state=INITIALSTATE;
-
-  return;
-
 }
 
 
-void nudge() {
-
-  lightEmUp(8);
-  beep(speakerPin,50,500);	
-  beep(speakerPin,100,500);	
-  off();	
-
-
-  return;
-
-}
-void sense() {
-  // send sensor values:
-  //if (DEBUG) 
-  Serial.write(state);
-}
-
+// update: shows the sensors readings of the other person's device 
 void update() {
 
-  int fuelLevel = inByte - 48;
+  // calc fuelLevel starting from 1
+  int fuelLevel = inByte - NUDGEMSG;
 
   if (DEBUG) Serial.println("fuel:" + fuelLevel);
 
-
+// set all pixels
   for(int i=0; i< 16; i++) {
     strip.setPixelColor(i, 0, 0, 0);
 
+    // turn on lower portion upto fuel level
     if (fuelLevel > i) {
+      // pick a nice color
       strip.setPixelColor(i, 30, 50, 60);
     }
 
-
   }
 
+  // communicate with pixels
   strip.show();
 
   return;
 
 }
 
+// make a sound (if speaker or buzzer) or vibration (if motor) or led flash at certain frequency.
+// this is nice for power savings and to minimize the current draws
+// i was a bit afraid of pulling too much current from my digial pins on the fio during testing with lots of different output devices
 void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)     // the sound producing function
 {	 
   int x;	 
@@ -201,6 +265,7 @@ void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMillisecon
   }	 
 }
 
+// sorta nice to see data upon completion of setup incase your loop is super screwed.
 void establishContact() {
   // say hello    
   Serial.write(INITIALSTATE);   // send an initial string
@@ -213,7 +278,7 @@ void establishContact() {
    */
 }
 
-
+// util to color all the leds
 void lightEmUp (int lights) {
 
   for(int i=0; i< 16; i++) {
@@ -232,13 +297,16 @@ void lightEmUp (int lights) {
 
 
 
-
+// util to off all the leds
 void off (){
 
   lightEmUp(0);
   return;
 
 }
+
+
+
 
 
 
